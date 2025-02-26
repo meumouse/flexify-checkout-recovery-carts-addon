218eb658882e82292f04a3b53645e00791ae0ab8
MeuMouse.com <contato@meumouse.com>
2025-02-26 10:58:35 -0300

    Composer installed for autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'meumouse/fc-recovery-carts',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '30ee4b1894fa20323b53d9684b49f73b5f1a6623',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'meumouse/fc-recovery-carts' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '30ee4b1894fa20323b53d9684b49f73b5f1a6623',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
